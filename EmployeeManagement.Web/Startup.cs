4a7f71b51481e0e492d61465b3ba4aab9805e619
yalcinPXL <yalcinPXL@gmail.com>
2021-07-06 11:19:03 +0200

    End of project

--- EmployeeManagement.Web/Startup.cs
+++ EmployeeManagement.Web/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
